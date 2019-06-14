a72b4b8479bccda37d5ccf429906576fe30c96c2
leonthemisfit <leonthemisfit@gmail.com>
2019-06-14 13:18:52 -0400

    Initial Commit
    The initial commit adds the basics needed to get started creating
    calsses in the mini FizzLang DSL for customizing FizzBuzz classes.
    
    On branch master
    Changes to be committed:
    	new file:   fizzlang.rb

---
+++ fizzlang.rb
@@ -0,0 +1,32 @@
+module FizzLang
+    def self.included(klass)
+        klass.extend(Fizzy)
+    end
+
+    def initialize(range)
+        @range = range
+    end
+end
+
+module Fizzy
+    def test_cases(*args)
+        @tests ||= nil
+        if @tests.nil?
+            @tests = {}
+            args.each do |sym|
+                define_singleton_method(sym) do |n|
+                    @tests[sym] ||= n
+                end
+            end
+        end
+        @tests
+    end
+
+    def check(n)
+        res = @tests.reduce('') do |acc, (sym, val)|
+            (n % val).zero? ? acc + sym.to_s : acc
+        end
+
+        res.empty? ? n.to_s : res
+    end
+end
