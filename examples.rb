63c4f2c1eb33fb1813412d7f59963b74146d1e3b
leonthemisfit <leonthemisfit@gmail.com>
2019-06-15 16:22:40 -0400

    Added a new example class for the keyword init features
    Very simple class in the same vein as the BlockBuzz class simply
    being used for demonstrating and testing the new keyword args
    features.
    
    On branch master
    Changes to be committed:
    	modified:   examples.rb

--- examples.rb
+++ examples.rb
@@ -58,3 +58,11 @@ class BlockBuzz
         cases[:Buzz] = 5
     end
 end
+
+# A class used to demonstrate a use of keyword arguments to
+# setup the class in place of class methods and blocks
+class KeyBuzz
+    include FizzLang
+
+    test_cases Key: 3, Buzz: 5
+end
