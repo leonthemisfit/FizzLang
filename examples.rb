ef82cad1fdf695cf0ba65ee314d26c0c73e3ba44
leonthemisfit <leonthemisfit@gmail.com>
2019-06-15 15:00:35 -0400

    Added a sample showing additional ways to use class methods
    This new example rather than using the class methods inside the
    definition of the class calls them outside of the class as methods.
    This might help show those not familiar with class methods their
    similarity to static methods in other languages and also demonstrate
    another way you may want to construct your classes.
    
    On branch master
    Changes to be committed:
    	modified:   examples.rb

--- examples.rb
+++ examples.rb
@@ -39,3 +39,11 @@ class FooBar
     Foo 5
     Bar 10
 end
+
+# A class used to demonstrate how class methods can be called
+# outside the class declaration and provide the same result.
+class Fizz
+    include FizzLang
+end
+Fizz.test_cases(:Fizz)
+Fizz.Fizz(3)
